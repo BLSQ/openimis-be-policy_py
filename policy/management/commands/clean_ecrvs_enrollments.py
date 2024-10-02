2f800dff9cbf8dd2530dbe908804702d2f41f541
Thibault Dethier <tdethier@bluesquarehub.com>
2024-10-02 19:06:25 +0200

    Disable clean_ecrvs_enrollments command

--- policy/management/commands/clean_ecrvs_enrollments.py
+++ policy/management/commands/clean_ecrvs_enrollments.py
@@ -63,73 +63,79 @@ def update_policy(policy: Policy, product_id: int, enroll_date, start_date, effe
     policy.status = new_status
     policy.save()
 
+HELP_TEXT = """
+    This command will clean all automatic enrollments that were made when uploading the eCRVS initial CSV data dump (circa July 2023).
+    There was only one Product at that time (the default paid one), so +65 and -5 people were enrolled in it. Policy duration dates were therefore also wrong.
+    This command will redispatch these people into their correct Product and will also correct policy dates.
+    This command is meant as a one shot - once it has been run, it shouldn't be run anymore
+    """
+
 
 class Command(BaseCommand):
-    help = ("This command will clean all automatic enrollments that were made when uploading the eCRVS initial CSV data dump (circa July 2023)."
-            "There was only one Product at that time (the default paid one), so +65 and -5 people were enrolled in it. Policy duration dates were therefore also wrong."
-            "This command will redispatch these people into their correct Product and will also correct policy dates."
-            "This command is meant as a one shot - once it has been run, it shouldn't be run anymore")
+    help = (HELP_TEXT)
 
     def handle(self, *args, **options):
         logger.info("Clean eCRVS enrollments command triggered")
+        print(HELP_TEXT)
+        return
 
-        logger.info("*** Fetching products ***")
-        elderly_product_id = Product.objects.filter(validity_to__isnull=True, code="BASIC65+").first().id
-        children_product_id = Product.objects.filter(validity_to__isnull=True, code="BASIC-5").first().id
-        default_product_id = Product.objects.filter(validity_to__isnull=True, code="BASIC").first().id
-
-        logger.info("*** Fetching policies ***")
-        policies = (Policy.objects.filter(Q(validity_to__isnull=True),
-                                          Q(product_id=default_product_id),
-                                          ~Q(status=Policy.STATUS_IDLE))
-                                  .select_related("family__head_insuree")
-                                  .prefetch_related("premiums", "insuree_policies")
-                                  .order_by("id"))
-
-        total_policies = 0
-        total_children = 0
-        total_elderly = 0
-        total_pages = 0
-        errors = []
-
-        paginator = Paginator(policies, UPDATE_PAGE_SIZE)
-        for page_number in paginator.page_range:
-            page = paginator.page(page_number)
-
-            total_pages += 1
-            logger.info(f"\t Processing policy batch {total_pages}")
-
-            for policy in page.object_list:
-                insuree = policy.family.head_insuree
-
-                if policy.premiums.exists():
-                    logger.info(f"\t Error policy ID {policy.id} - there is a Premium for this policy! Insuree NIN = {insuree.chf_id}")
-                    errors.append((policy.id, insuree.chf_id, "existing premium"))
-
-                age_at_enrollment = insuree.age(policy.enroll_date)
-
-                if age_at_enrollment < 5:
-                    update_policy_for_children(policy, insuree, children_product_id)
-                    total_children += 1
-                elif age_at_enrollment >= 65:
-                    update_policy_for_elderly(policy, insuree, elderly_product_id)
-                    total_elderly += 1
-                else:
-                    logger.info(f"\t Error policy ID {policy.id} "
-                                f"- the Insuree's ({insuree.chf_id}) age at enrollment is incorrect: {age_at_enrollment} "
-                                f"- DOB={insuree.dob} - enrollment date={policy.enroll_date}")
-                    errors.append((policy.id,
-                                   insuree.chf_id,
-                                   f"incorrect age ({age_at_enrollment}) - DOB={insuree.dob} - enrollment date={policy.enroll_date}"))
-
-                total_policies += 1
-
-        logger.info("**********************************")
-        logger.info("Results:")
-        logger.info(f"\t total policies: {total_policies}")
-        logger.info(f"\t total children: {total_children}")
-        logger.info(f"\t total elderly: {total_elderly}")
-        logger.info(f"\t total errors: {len(errors)}")
-        logger.info(f"\t detail errors:")
-        for error in errors:
-            logger.info(f"\t\t error policy {error[0]} - insuree {error[1]} - reason: {error[2]}")
+        # logger.info("*** Fetching products ***")
+        # elderly_product_id = Product.objects.filter(validity_to__isnull=True, code="BASIC65+").first().id
+        # children_product_id = Product.objects.filter(validity_to__isnull=True, code="BASIC-5").first().id
+        # default_product_id = Product.objects.filter(validity_to__isnull=True, code="BASIC").first().id
+        #
+        # logger.info("*** Fetching policies ***")
+        # policies = (Policy.objects.filter(Q(validity_to__isnull=True),
+        #                                   Q(product_id=default_product_id),
+        #                                   ~Q(status=Policy.STATUS_IDLE))
+        #                           .select_related("family__head_insuree")
+        #                           .prefetch_related("premiums", "insuree_policies")
+        #                           .order_by("id"))
+        #
+        # total_policies = 0
+        # total_children = 0
+        # total_elderly = 0
+        # total_pages = 0
+        # errors = []
+        #
+        # paginator = Paginator(policies, UPDATE_PAGE_SIZE)
+        # for page_number in paginator.page_range:
+        #     page = paginator.page(page_number)
+        #
+        #     total_pages += 1
+        #     logger.info(f"\t Processing policy batch {total_pages}")
+        #
+        #     for policy in page.object_list:
+        #         insuree = policy.family.head_insuree
+        #
+        #         if policy.premiums.exists():
+        #             logger.info(f"\t Error policy ID {policy.id} - there is a Premium for this policy! Insuree NIN = {insuree.chf_id}")
+        #             errors.append((policy.id, insuree.chf_id, "existing premium"))
+        #
+        #         age_at_enrollment = insuree.age(policy.enroll_date)
+        #
+        #         if age_at_enrollment < 5:
+        #             update_policy_for_children(policy, insuree, children_product_id)
+        #             total_children += 1
+        #         elif age_at_enrollment >= 65:
+        #             update_policy_for_elderly(policy, insuree, elderly_product_id)
+        #             total_elderly += 1
+        #         else:
+        #             logger.info(f"\t Error policy ID {policy.id} "
+        #                         f"- the Insuree's ({insuree.chf_id}) age at enrollment is incorrect: {age_at_enrollment} "
+        #                         f"- DOB={insuree.dob} - enrollment date={policy.enroll_date}")
+        #             errors.append((policy.id,
+        #                            insuree.chf_id,
+        #                            f"incorrect age ({age_at_enrollment}) - DOB={insuree.dob} - enrollment date={policy.enroll_date}"))
+        #
+        #         total_policies += 1
+        #
+        # logger.info("**********************************")
+        # logger.info("Results:")
+        # logger.info(f"\t total policies: {total_policies}")
+        # logger.info(f"\t total children: {total_children}")
+        # logger.info(f"\t total elderly: {total_elderly}")
+        # logger.info(f"\t total errors: {len(errors)}")
+        # logger.info(f"\t detail errors:")
+        # for error in errors:
+        #     logger.info(f"\t\t error policy {error[0]} - insuree {error[1]} - reason: {error[2]}")
